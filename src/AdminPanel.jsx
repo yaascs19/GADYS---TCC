787e34cabf16bd06f7893da712c6afd89cc42485
yasmincunegundes25 <yasmincunegundes25@gmail.com>
2026-04-04 17:29:15 +0000

    Fix: estilização admin painel 14

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './AdminPanel.css'
-import '../App.css'
+import './App.css'
 
 function AdminPanel() {
   const navigate = useNavigate()
